43383ab0aad8c7453f4fab3dc5489740ccbb7dd8
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-01-17 21:25:39 +0200

    Schedule page

--- src/components/Schedule/SchedulePage.vue
+++ src/components/Schedule/SchedulePage.vue
@@ -1,13 +1,28 @@
 <template>
-
+  <div class="schedulePageWrapper">
+    <DayOfWeek day-of-week="Monday" :lessons="[]"/>
+    <DayOfWeek day-of-week="Tuesday" :lessons="[]"/>
+    <DayOfWeek day-of-week="Wednesday" :lessons="[]"/>
+    <DayOfWeek day-of-week="Thursday" :lessons="[]"/>
+    <DayOfWeek day-of-week="Friday" :lessons="[]"/>
+    <DayOfWeek day-of-week="Saturday" :lessons="[]"/>
+    <DayOfWeek day-of-week="Sunday" :lessons="[]"/>
+  </div>
 </template>
 
 <script>
+import DayOfWeek from "./components/DayOfWeek.vue";
+
 export default {
-  name: "SchedulePage"
+  name: "SchedulePage",
+  components: {DayOfWeek}
 }
 </script>
 
 <style scoped>
+
+.schedulePageWrapper {
+  display: flex;
+}
 
 </style>
