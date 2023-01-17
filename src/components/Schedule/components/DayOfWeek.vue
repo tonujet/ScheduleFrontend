2f91907080108bb50dcd8f9f91419c78b663f493
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-01-17 20:56:25 +0200

    Lesson

---
+++ src/components/Schedule/components/DayOfWeek.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "DayOfWeek"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Schedule/components/components/Lesson.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <p>
+    name: {{name}}
+  </p>
+  <p>
+    day of week: {{dayOfWeek}}
+  </p>
+  <p>
+    time: {{time}}
+  </p>
+  <p>
+    classroom: {{classroom}}
+  </p>
+  <p>
+    teacher: {{`${teacher.name} ${teacher.surname}`}}
+  </p>
+  <p>
+    discipline: {{discipline}}
+  </p>
+  <p>
+    group: {{group}}
+  </p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Lesson",
+  props: {
+    name: String,
+    dayOfWeek: String,
+    time: String,
+    classroom: String,
+    teacher: Object,
+    discipline: Object,
+    group: Object,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
