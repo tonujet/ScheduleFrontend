ac4ab7b79341c70f1f704406d1cc2a8b9016538f
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-01-17 21:16:19 +0200

    Day of week

--- src/components/Schedule/components/DayOfWeek.vue
+++ src/components/Schedule/components/DayOfWeek.vue
@@ -1,13 +1,35 @@
 <template>
-
+  <div class="lessonsWrapper">
+    <p>{{dayOfWeek}}</p>
+    <Lesson v-for="{id, name,
+    dayOfWeek,
+    time,
+    classroom,
+    teacher,
+    discipline,
+    group} in lessons" :key="id" :name="name" :dayOfWeek="dayOfWeek" :time="time" , :classroom="classroom"
+            :teacher="teacher" :discipline="discipline" :group="group">
+    </Lesson>
+  </div>
 </template>
 
 <script>
+import Lesson from "./components/Lesson.vue";
+
 export default {
-  name: "DayOfWeek"
+  name: "DayOfWeek",
+  components: {Lesson},
+  props: {
+    dayOfWeek: String,
+    lessons: Array,
+  }
 }
 </script>
 
 <style scoped>
+
+.lessonsWrapper {
+
+}
 
 </style>
