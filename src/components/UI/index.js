f76428785eb321dcb2eea642ba02a6b782314d39
tonujet <tonujet@gmail.com>
2023-01-18 18:41:30 +0100

    Did all UI components as global components

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,6 +1,13 @@
 import ModalWindow from "@/components/UI/ModalWindow.vue";
+import CustomInput from "@/components/UI/CustomInput.vue";
+import CustomTextarea from "@/components/UI/CustomTextarea.vue";
+import CustomDate from "@/components/UI/CustomDate.vue";
+import CustomSelect from "@/components/UI/CustomSelect.vue";
 
 export default[
     ModalWindow,
-
+    CustomTextarea,
+    CustomDate,
+    CustomSelect,
+    CustomInput
 ]
