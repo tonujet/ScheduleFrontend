776c8d1b2dc025aeedab4a964cbbe3a3390b505e
tonujet <tonujet@gmail.com>
2023-01-18 13:23:09 +0100

    Option mock update

--- src/mocks/options.js
+++ src/mocks/options.js
@@ -1,78 +1,214 @@
 export default {
     "schedules": [
-        {"name": "name", "type": "String", "data": null, "nullable": false}, {
+        {
+            "name": "name",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
+            "nullable": false
+        },
+        {
             "name": "teacher",
-            "type": "ENUM",
-            "data": "teachers",
+            "inputType": "data",
+            "dataSource": "teachers",
+            "validatorName": null,
             "nullable": true
-
-        }, {"name": "discipline", "type": "ENUM", "data": "disciplines", "nullable": true}, {
+        },
+        {
+            "name": "discipline",
+            "inputType": "data",
+            "dataSource": "disciplines",
+            "validatorName": null,
+            "nullable": true
+        },
+        {
             "name": "dayOfWeek",
-            "type": "weekDays",
-            "data": null,
+            "inputType": "selectWeekDays",
+            "dataSource": null,
+            "validatorName": null,
             "nullable": true
-        }, {"name": "time", "type": "ENUM", "data": "startsTime", "nullable": true}, {
+        },
+        {
+            "name": "time",
+            "inputType": "data",
+            "dataSource": "startsTime",
+            "validatorName": null,
+            "nullable": true
+        },
+        {
             "name": "classroom",
-            "type": "String",
-            "data": null,
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textNumberDashValidator",
             "nullable": true
-        }, {"name": "group", "type": "ENUM", "data": "groups", "nullable": true}],
-    "disciplines": [{"name": "name", "type": "String", "data": null, "nullable": false}],
-    "groups": [
-        {"name": "name", "type": "String", "data": null, "nullable": false}, {
-            "name": "department",
-            "type": "ENUM",
-            "data": "departments",
-            "nullable": true
-        }, {"name": "course", "type": "String", "data": null, "nullable": true}],
-    "students": [
-        {"name": "name", "type": "String", "data": null, "nullable": false}, {
+        },
+        {
             "name": "group",
-            "type": "ENUM",
-            "data": "groups",
+            "inputType": "data",
+            "dataSource": "groups",
+            "validatorName": null,
             "nullable": true
-        }, {"name": "email", "type": "String", "data": null, "nullable": false}, {
-            "name": "phone",
-            "type": "String",
-            "data": null,
+        }
+    ],
+    "disciplines": [
+        {
+            "name": "name",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
             "nullable": false
-        }],
+        }
+    ],
+    "groups": [
+        {
+            "name": "name",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
+            "nullable": false
+        },
+        {
+            "name": "department",
+            "inputType": "data",
+            "dataSource": "departments",
+            "validatorName": null,
+            "nullable": true
+        },
+        {
+            "name": "course",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "courseValidator",
+            "nullable": true
+        }
+    ],
+    "students": [
+        {
+            "name": "name",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
+            "nullable": false
+        },
+        {
+            "name": "group",
+            "inputType": "data",
+            "dataSource": "groups",
+            "validatorName": null,
+            "nullable": true
+        },
+        {
+            "name": "email",
+            "inputType": "email",
+            "dataSource": null,
+            "validatorName": "emailValidator",
+            "nullable": false
+        },
+        {
+            "name": "phone",
+            "inputType": "phone",
+            "dataSource": null,
+            "validatorName": "phoneValidator",
+            "nullable": false
+        }
+    ],
     "teachers": [
-        {"name": "name", "type": "String", "data": null, "nullable": false}, {
+        {
+            "name": "name",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
+            "nullable": false
+        },
+        {
             "name": "surname",
-            "type": "String",
-            "data": null,
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
             "nullable": false
-        }, {"name": "email", "type": "String", "data": null, "nullable": false}, {
+        },
+        {
+            "name": "email",
+            "inputType": "email",
+            "dataSource": null,
+            "validatorName": "emailValidator",
+            "nullable": false
+        },
+        {
             "name": "phone",
-            "type": "String",
-            "data": null,
+            "inputType": "phone",
+            "dataSource": null,
+            "validatorName": "phoneValidator",
             "nullable": false
-        }],
+        }
+    ],
     "departments": [
-        {"name": "name", "type": "String", "data": null, "nullable": false}, {
+        {
+            "name": "name",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
+            "nullable": false
+        },
+        {
             "name": "shortName",
-            "type": "String",
-            "data": null,
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textNumberValidator",
             "nullable": false
-        }, {"name": "faculty", "type": "ENUM", "data": "faculties", "nullable": true}],
-    "news": [
-        {"name": "title", "type": "String", "data": null, "nullable": false}, {
-            "name": "description",
-            "type": "String",
-            "data": null,
-            "nullable": false
-        }, {"name": "text", "type": "Text", "data": null, "nullable": false}, {
-            "name": "dateCreated",
-            "type": "DateValue",
-            "data": null,
+        },
+        {
+            "name": "faculty",
+            "inputType": "data",
+            "dataSource": "faculties",
+            "validatorName": null,
             "nullable": true
-        }, {"name": "headerImg", "type": "File", "data": null, "nullable": false}],
-    "faculties": [
-        {"name": "name", "type": "String", "data": null, "nullable": false}, {
-            "name": "shortName",
-            "type": "String",
-            "data": null,
+        }
+    ],
+    "news": [
+        {
+            "name": "title",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": null,
             "nullable": false
-        }]
+        },
+        {
+            "name": "description",
+            "inputType": "textarea",
+            "dataSource": null,
+            "validatorName": null,
+            "nullable": false
+        },
+        {
+            "name": "text",
+            "inputType": "textarea",
+            "dataSource": null,
+            "validatorName": null,
+            "nullable": false
+        },
+        {
+            "name": "dateCreated",
+            "inputType": "date",
+            "dataSource": null,
+            "validatorName": "dateValidator",
+            "nullable": true
+        }
+    ],
+    "faculties": [
+        {
+            "name": "name",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
+            "nullable": false
+        },
+        {
+            "name": "shortName",
+            "inputType": "text",
+            "dataSource": null,
+            "validatorName": "textOnly",
+            "nullable": false
+        }
+    ]
 }
