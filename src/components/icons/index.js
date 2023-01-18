705884863cf9d0d42f427be9f0ff00c336f7e425
tonujet <tonujet@gmail.com>
2023-01-18 01:19:51 +0100

    Add font awesome icons for CRUD

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,12 @@
-export default [
+import {
+    faTrashCan,
+    faCirclePlus,
+    faPenNib
 
+}from "@fortawesome/free-solid-svg-icons";
+
+export default [
+    faCirclePlus,
+    faTrashCan,
+    faPenNib
 ]
