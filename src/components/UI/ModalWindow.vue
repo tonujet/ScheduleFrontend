4e96f9a7172155c91346bb48da1d13cf705e4f0e
tonujet <tonujet@gmail.com>
2023-01-18 01:51:58 +0100

    Created flexible modal window

---
+++ src/components/UI/ModalWindow.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="modalWindow" v-if="isOpen">
+    <div class="modalWindow__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalWindow",
+  props:{
+    isOpen:{
+      type:Boolean,
+      default:false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.modalWindow{
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0 , 0.8);
+  display: flex;
+  position: fixed;
+}
+.modalWindow__content{
+  position: relative;
+  background: white;
+  height: 300px;
+  width: 300px;
+}
+</style>
