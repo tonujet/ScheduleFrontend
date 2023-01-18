9eadd4a971e6bc343b531bf0a9415cec0af49149
tonujet <tonujet@gmail.com>
2023-01-18 18:29:52 +0100

    Create CustomDate.vue

---
+++ src/components/UI/CustomDate.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+      class="date"
+      type="datetime-local"
+      @change="$emit('update:modelValue',$event.target.value)">
+</template>
+
+<script>
+export default {
+  name: "DateInput"
+}
+</script>
+
+<style scoped>
+.date{
+
+}
+
+
+</style>
