338274ce37e1a368cf7e5a348ed17b357ef3463c
tonujet <tonujet@gmail.com>
2023-01-19 02:05:17 +0100

    Created constant folder and added day of week to it

---
+++ src/assets/constants/WeekDays.js
@@ -0,0 +1,9 @@
+export default {
+    "MONDAY":"Понеділок",
+    "TUESDAY":"Вівторок",
+    "WEDNESDAY":"Середа",
+    "THURSDAY":"Четвер",
+    "FRIDAY":"П'ятниця",
+    "SATURDAY":"Субота",
+    "SUNDAY":"Неділя",
+}
