5c8f39a34d0b92b65625c6a22f0bfe7a3e051e58
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-01-19 11:57:29 +0200

    Schedule search by groups is done

--- src/components/Schedule/SchedulePage.vue
+++ src/components/Schedule/SchedulePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="schedulePageWrapper">
     <v-select v-if="groups.value.length" :options="groups.value" label="name" :reduce="group => group.id"
-              @update:modelValue="handleGroupSelect"/>
+              @update:modelValue="handleGroupSelect" :modelValue="groupsIdParameter.value"/>
     <h2 v-else class="noGroupsWarning">Немає груп для пошуку розкладу</h2>
     <div class="schedulePage">
       <DayOfWeek v-for="dayOfWeek in daysOfWeek"
@@ -20,21 +20,22 @@ export default {
   name: "SchedulePage",
   components: {DayOfWeek},
   computed: {
+    groupsIdParameter: () => groupsIdParameter,
     groups: () => groups,
     schedulesRecords: () => schedule,
     daysOfWeek: () => Array.from(new Set(schedule.value.map((dayOfWeek) => dayOfWeek.dayOfWeek))).filter(el => el)
   },
   methods: {
     async handleGroupSelect(groupId) {
-      console.log(groupId)
-      const {data} = await this.$api.scheduleDays.getSchedule(groupId)
+      groupsIdParameter.value = groupId
+      const {data} = await this.$api.scheduleDays.getSchedule(groupsIdParameter.value)
       schedule.value = data
     }
   },
   mounted() {
     this.$load(async () => {
       const {data: groupsData} = await this.$api.groups.getGroups()
-      // groups.value = groupsData;
+      groups.value = groupsData;
 
       if (groups.value.length) {
         const {data: scheduleData} = await this.$api.scheduleDays.getSchedule(groups.value[0].id)
@@ -44,12 +45,8 @@ export default {
   }
 }
 
-const groups = ref([
-  {id: 1, name: "IA-11"},
-  {id: 2, name: "IA-12"},
-  {id: 3, name: "IA-13"},
-  {id: 4, name: "IA-14"}
-]);
+const groups = ref([]);
+const groupsIdParameter = ref(groups.value.length ? groups.value[0].id : null);
 const schedule = ref([]);
 
 </script>
